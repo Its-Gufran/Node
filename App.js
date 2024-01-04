6ac5926c8826ffb5d12f8701686ee7a4dac758b1
iamgufran <mgfzd786@gmail.com>
2024-01-04 21:56:24 +0530

    From node to express

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-// http -> launches a server and send requests
-// https-> launches a SSL server
-const http = require('http')
-const routes = require('./routes')
 
-// function inside a createServer is a request listener and is executed whenever a request comes.
-const server = http.createServer(routes.handler)
-    //console.log(req)
-    //to get exit from the server
-    //process.exit()
+const express = require('express')
 
+const app = express()
 
-server.listen(3000)
+//use allows us to add middleware functions
+//next is used to go to next middleware
+app.use('/',(req, res, next) => {
+    console.log('First middleware')
+    next()
+})
 
-//Nodejs is single threaded even then it handles multiple requests
-//Event loop handles all the event listers i.e. easy tasks and all heavy lifting tasks are handled by worker pool(handles by Nodejs and runs on different thread)
-//Callback by worker pool are handled by event loop
+app.use((req, res, next) => {
+    console.log('second middleware')
+    res.send('<h1>Hello from Express</h1>')
+})
+
+//creates the server and passes itself
+app.listen(3000);
+
