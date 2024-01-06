a2ca612134edbf05bc93cfa60550fca6cb35818b
iamgufran <mgfzd786@gmail.com>
2024-01-06 17:24:04 +0530

    Filtering middlewares

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
+const express = require("express");
+const bodyParser = require('body-parser')
 
-const express = require('express')
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 
-const app = express()
-
+//middleware
+//next is used to go to next middleware
 //use allows us to add middleware functions
 //next is used to go to next middleware
-app.use('/',(req, res, next) => {
-    console.log('First middleware')
-    next()
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action = "/product" method = "POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></input></form>'
+  );
+});
 
+//Now this middleware will run only in cases of post request and similarly we can make it for only get requests
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
+app.use("/", (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
 //creates the server and passes itself
 app.listen(3000);
-
