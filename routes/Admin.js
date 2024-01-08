f40b13dc42bb42e66565f378d6cb52d279f0ebbd
iamgufran <mgfzd786@gmail.com>
2024-01-08 19:20:52 +0530

    Add Utils and Styling HTML pages

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,10 +1,11 @@
 const express = require("express")
-
+const path = require("path")
+const rootDir =require('../utils/path')
 const router = express.Router();
 
+
 router.get("/add-product", (req, res, next) => {
-    res.send(
-      '<form action = "/admin/add-product" method = "POST"><input type = "text" name = "title"><button type = "submit">Add Product</button></input></form>'
+    res.sendFile(path.join(rootDir, 'Views', 'add-product.html')
     );
   });
   
