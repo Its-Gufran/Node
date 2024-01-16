0b5bd046cd2269cdf19833ff8be35d3e5dce71a5
iamgufran <mgfzd786@gmail.com>
2024-01-16 19:07:43 +0530

    Convert to ejs

--- App.js
+++ App.js
@@ -3,18 +3,22 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const adminData = require("./routes/Admin");
 const shopRoutes = require("./routes/Shop");
+// const expressHbs = require("express-handlebars");
+
 const app = express();
-app.set('view engine', 'pug');
-app.set('views','views')
+
+// app.engine('hbs', expressHbs());
+app.set('view engine', "ejs");
+app.set('views', "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
- 
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404')
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 //creates the server and passes itself
